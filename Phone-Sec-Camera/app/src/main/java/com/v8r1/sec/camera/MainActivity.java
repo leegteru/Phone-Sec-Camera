8db08eebbc2f51772dbcf96375a5bbef30c5c3a0
leegteru <leegteru@gmail.com>
2017-08-27 23:38:44 -0400

    Test Commit to check the gerrit push process

--- Phone-Sec-Camera/app/src/main/java/com/v8r1/sec/camera/MainActivity.java
+++ Phone-Sec-Camera/app/src/main/java/com/v8r1/sec/camera/MainActivity.java
@@ -3,6 +3,7 @@ package com.v8r1.sec.camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*Main Activity class*/
 public class MainActivity extends AppCompatActivity {
 
     @Override
